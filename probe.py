7ca58d3f830c7bf66b7a8e71617437d5ee3a5793
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-12 15:00:31 +0200

    added raw network detection

--- probe.py
+++ probe.py
@@ -9,23 +9,22 @@ class Probe():
         self.name = "probe"
         self.ip_list = ['1.1.1.1', '1.0.0.1']
         self.count = 1
+        self.time_interval = 2
 
     def run_probes(self):
         for ip in self.ip_list:
             result = self.ping(ip)
             print(result)
+        time.sleep(self.time_interval)
 
     def ping(self, ip):
         """
-        sends a PING to IP and returns
-            "target": IP,
-            "up": 1 or 0,
-            "timestamp": POSIX (ms)
+        returns:
+        {"target": IP, "up": 1 or 0, "timestamp": POSIX(ms)}
         """
 
         posix = time.time() * 1000
         response = os.popen(f"ping -n {self.count} {ip}").read()
-        # print(response)
         if (f"Received = {self.count}") in response:
             up = 1
         else:
@@ -38,7 +37,18 @@ class Probe():
         }
         return result
 
+    def detect_network(self):
+        "Trace the first nodes of the network"
+        trace_ips = []
+        response = os.popen(f"pathping -q 1 1.1.1.1").readlines()
+        for line in response:
+            if len(line) > 3 and line[2] in ["1", "2", "3"]:
+                trace_ip = line.rsplit("[")[1][0:-3]
+                print(trace_ip)
+
 
 if __name__ == "__main__":
     probe = Probe()
-    probe.run_probes()
+    for i in range(3):
+        probe.run_probes()
+    # probe.detect_network()
