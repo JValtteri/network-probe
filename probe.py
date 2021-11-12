483718331f0e77535b7293e810cf975938ab8e62
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-12 14:25:43 +0200

    Probe as class

--- probe.py
+++ probe.py
@@ -1,10 +1,44 @@
 import os
+import time
 
-ip_list = ['8.8.8.8']
-for ip in ip_list:
-    response = os.popen(f"ping {ip}").read()
-    print(response)
-    if "Received = 4" in response:
-        print(f"UP {ip} Ping Successful")
-    else:
-        print(f"DOWN {ip} Ping Unsuccessful")
+class Probe():
+
+    def __init__(self):
+
+        self.id = 0
+        self.name = "probe"
+        self.ip_list = ['1.1.1.1', '1.0.0.1']
+        self.count = 1
+
+    def run_probes(self):
+        for ip in self.ip_list:
+            result = self.ping(ip)
+            print(result)
+
+    def ping(self, ip):
+        """
+        sends a PING to IP and returns
+            "target": IP,
+            "up": 1 or 0,
+            "timestamp": POSIX (ms)
+        """
+
+        posix = time.time() * 1000
+        response = os.popen(f"ping -n {self.count} {ip}").read()
+        # print(response)
+        if (f"Received = {self.count}") in response:
+            up = 1
+        else:
+            up = 0
+
+        result = {
+            "target": ip,
+            "up": up,
+            "timestamp": posix
+        }
+        return result
+
+
+if __name__ == "__main__":
+    probe = Probe()
+    probe.run_probes()
