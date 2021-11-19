6cb192d8559aa1363cb3cd6f3244424ea41486cd
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-19 21:14:28 +0200

    Disable logging to file

--- logger.py
+++ logger.py
@@ -24,23 +24,23 @@ class Logger():
         self.logger.setLevel(logging.DEBUG)
 
         # Create handlers
-        self.file_handler = logging.FileHandler(filename)
+        # self.file_handler = logging.FileHandler(filename)
         self.stream_handler = logging.StreamHandler()
 
         # Set handler levels
         self.stream_handler.setLevel(logging.DEBUG)
-        self.file_handler.setLevel(logging.INFO)
+        # self.file_handler.setLevel(logging.INFO)
 
         # Set log format
         self.logging_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
         # Add formatter to handlers
         self.stream_handler.setFormatter(self.logging_format)
-        self.file_handler.setFormatter(self.logging_format)
+        # self.file_handler.setFormatter(self.logging_format)
 
         # Add handlers to the logger
         self.logger.addHandler(self.stream_handler)
-        self.logger.addHandler(self.file_handler)
+        # self.logger.addHandler(self.file_handler)
 
     def get_logger(self):
         '''
